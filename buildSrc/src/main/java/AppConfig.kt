725a8978f27061823041054a5341289d1df4fa6b
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-06-20 14:30:50 +0200

    Update AppConfig.kt

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,14 +2,14 @@ import org.gradle.api.JavaVersion
 
 // App level config constants
 object AppConfig {
-    const val compileSdk = 32
+    const val compileSdk = 35
     const val minSdk = 23
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val applicationId = "com.abhishek.pathak.kotlin.android.githubcompose"
     const val versionCode = 1
     const val versionName = "1.0.0"
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 
-    val javaJvmTarget = JavaVersion.VERSION_11.toString()
-    val javaCompatibility = JavaVersion.VERSION_11
+    val javaJvmTarget = JavaVersion.VERSION_17.toString()
+    val javaCompatibility = JavaVersion.VERSION_17
 }
